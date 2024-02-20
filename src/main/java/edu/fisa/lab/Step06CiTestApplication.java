9eeb62e55a4e450b5e27ad408871aac8c7cf372c
parfait2 <parkyl0208@naver.com>
2024-02-20 10:10:58 +0900

    [Add] ci test project

---
+++ src/main/java/edu/fisa/lab/Step06CiTestApplication.java
@@ -0,0 +1,13 @@
+package edu.fisa.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step06CiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Step06CiTestApplication.class, args);
+	}
+
+}
