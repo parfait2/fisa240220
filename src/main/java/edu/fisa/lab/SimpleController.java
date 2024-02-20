2e9bcc6d652980732b1af2384ca003631ebfd9e8
parfait2 <parkyl0208@naver.com>
2024-02-20 11:18:57 +0900

    [Update] 수정

---
+++ src/main/java/edu/fisa/lab/SimpleController.java
@@ -0,0 +1,13 @@
+package edu.fisa.lab;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+public class SimpleController {
+
+	@GetMapping("getdata")
+	public String get() {
+		return "get data - 수정 1";
+	}
+}
